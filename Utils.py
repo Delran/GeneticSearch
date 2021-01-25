5f40422f08558a9b56dd745b2b2d3eb84eccb565
Tibère Borel <tibere.borel@setec.com>
2021-01-25 16:13:46 +0100

    Working algorithm

---
+++ Utils.py
@@ -0,0 +1,87 @@
+"""Util functions for genetic search algorithm."""
+import string
+import random
+
+# Storing the length of the printable const
+nbPrintables = len(string.printable)
+
+
+def getRandomPrintable():
+    """Return a random printable char."""
+    ret = ""
+    while True:
+        index = random.randrange(nbPrintables)
+        ret = string.printable[index]
+        if not ret.isspace() or ret == " ":
+            break
+    return ret
+
+
+def generateRandomString(length):
+    """Generate a random string with the given length."""
+    randomStr = ""
+    for i in range(length):
+        randomStr += getRandomPrintable()
+    return randomStr
+
+
+def mixStrings(str1, str2):
+    """Randomly mixes two given string."""
+    ret = ""
+    length = len(str1)
+    rand = random.randint(1, length-1)
+    for i in range(length):
+        char = str1[i] if i < rand else str2[i]
+        ret += char
+    return ret
+
+
+def changeOneChar(str):
+    """Change a random char of the given string to another printable."""
+    ret = ""
+    length = len(str)
+    rand = random.randrange(length)
+    for i in range(length):
+        char = str[i] if i != rand else getRandomPrintable()
+        ret += char
+    return ret
+
+
+def quickSort(list, sortFn):
+    """Quick sort with sorting function."""
+    """Given list will be sorted using the function passed in argument"""
+    """The sort isn't in place and will use additional memory"""
+
+    # TODO: Should sorting function do the comparison between the items ?
+
+    # Using python lists as sub arrays
+    greater = []
+    equal = []
+    lesser = []
+
+    # Used to count the number of matched items
+    # small optimization to avoid using len()
+    nbAbove = 0
+    nbBelow = 0
+
+    pivot = sortFn(list[0])
+    for item in list:
+        # Getting sorting function result for current item
+        i = sortFn(item)
+        if i < pivot:
+            nbAbove += 1
+            greater.append(item)
+        elif i > pivot:
+            nbBelow += 1
+            lesser.append(item)
+        else:
+            equal.append(item)
+
+    # Recursively sorting the greater and less item lists
+    # only if there is more than one element
+    sortedLow = quickSort(lesser, sortFn) if nbBelow > 1 else lesser
+    sortedHigh = quickSort(greater, sortFn) if nbAbove > 1 else greater
+
+    # Equals items doesn't need to be sorted
+    # Using python easy list merging to avoid headaches
+    return sortedLow + equal + sortedHigh
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+"""Main file for Genetic search algorithm."""
+from GeneticSearch import GeneticSearch
 
+
+def main():
+    """Entry point function for genetic search algorithm."""
+    population = 500
+    mutationRate = 0.5
+    selectionRate = 0.25
+    search = GeneticSearch("Je cherche cette phrase",
+                           population, mutationRate, selectionRate)
+    search.start()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
